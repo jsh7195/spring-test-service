9143a6848148f0b704b4cfe30de7ecee2c62c640
jsh7195 <jsh7195gg@gmail.com>
2021-04-22 16:39:31 +0900

    init setting

--- src/main/java/com/example/sampleApi/controller/sampleController.java
+++ src/main/java/com/example/sampleApi/controller/sampleController.java
@@ -3,23 +3,30 @@ package com.example.sampleApi.controller;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 @RestController
-@RequestMapping(value = "/test/")
+@RequestMapping(value = "/test")
 public class sampleController {
-    @RequestMapping(value = "/testapi", method = RequestMethod.GET)
+    private static final Logger log = LoggerFactory.getLogger(sampleController.class);
+    
+    @GetMapping("/testapi")
     ResponseEntity<Map<String, String>> sample() {
         ResponseEntity<Map<String, String>> response = null;
 
         Map<String, String> resMap = new HashMap<String, String>();
         resMap.put("type", "First eureka client!");
         resMap.put("message", "Spring Cloud is awesome!");
-
+        log.debug("test");
         response = new ResponseEntity<Map<String, String>>(resMap, HttpStatus.OK);
 
         return response;
